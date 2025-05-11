f07802d60fad85b89170d9263278bf43344d03be
kobly <nayun9191@gmail.com>
2025-05-11 19:17:03 +0900

    vercel 배포 오류

--- src/app/profile/[username]/edit/page.tsx
+++ src/app/profile/[username]/edit/page.tsx
@@ -2,17 +2,16 @@ import { getSession } from "@/lib/session";
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
 import EditForm from "./editForm";
+import { useParams } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
-export default async function EditProfilePage({
-  params,
-}: {
-  params: Promise<{ username: string }>;
-}) {
-  const { username } = await params;
-  const session = await getSession();
+export default async function EditProfilePage() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const params = useParams();
+  const username = (params?.username || "") as string;
 
+  const session = await getSession();
   const user = await db.user.findUnique({
     where: { id: session.id },
   });
