2f4bf698d3603e6f3d3581352fd51f09427c4a97
kobly <nayun9191@gmail.com>
2025-05-11 19:04:55 +0900

    vercel 배포 오류

--- src/app/profile/[username]/edit/page.tsx
+++ src/app/profile/[username]/edit/page.tsx
@@ -3,20 +3,16 @@ import db from "@/utils/db";
 import { redirect } from "next/navigation";
 import EditForm from "./editForm";
 
-export default async function EditProfilePage({
-  params,
-}: {
-  params: Promise<{ username: string }>;
+export default async function EditProfilePage(props: {
+  params: { username: string };
 }) {
-  const { username } = await params;
-
   const session = await getSession();
 
   const user = await db.user.findUnique({
     where: { id: session.id },
   });
 
-  if (!user || user.username !== username) {
+  if (!user || user.username !== props.params.username) {
     redirect("/");
   }
 
