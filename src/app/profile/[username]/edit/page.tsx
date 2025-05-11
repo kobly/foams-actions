5dab981810688b10e18c7f9a43640f21d4c9df20
kobly <nayun9191@gmail.com>
2025-05-11 18:47:34 +0900

    vercel 배포 오류

--- src/app/profile/[username]/edit/page.tsx
+++ src/app/profile/[username]/edit/page.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { getSession } from "@/lib/session";
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
 import EditForm from "./editForm";
 
-export default async function EditProfilePage({ params }: any) {
+export default async function EditProfilePage(props: any) {
   const session = await getSession();
+
+  const { username } = props.params;
 
   const user = await db.user.findUnique({
     where: { id: session.id },
   });
 
-  if (!user || user.username !== params.username) {
+  if (!user || user.username !== username) {
     redirect("/");
   }
 
